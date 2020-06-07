66786af95888291e916d71d7172d69ae58e5aea4
aluiziocatao <aluizio2210@gmail.com>
2020-06-06 19:23:39 -0500

    App

---
+++ server/src/database/migrations/01_create_items.ts
@@ -0,0 +1,12 @@
+import Knex from 'knex';
+export async function up(knex: Knex){ // CRIAR TABELA
+    return knex.schema.createTable('items', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+    });
+}
+
+export async function down(knex: Knex){ // APAGAR TABELA
+    return knex.schema.dropTable('items');
+}
